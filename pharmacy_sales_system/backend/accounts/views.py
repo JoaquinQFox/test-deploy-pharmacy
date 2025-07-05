9eb8654facfd2995b0f86b99893a699b02f0eebb
leonhatches <jhatches@unsa.edu.pe>
2025-07-05 16:47:48 -0500

    feat: se agrega nueva validación de campos vacíos

--- pharmacy_sales_system/backend/accounts/views.py
+++ pharmacy_sales_system/backend/accounts/views.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse
 from django.middleware.csrf import get_token
 from django.views.decorators.http import require_POST
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 import json
 
 @ensure_csrf_cookie
@@ -25,10 +26,11 @@ def registerView(request):
     confirms   = data.get("confirms")
     rol        = data.get("rol")
 
-    if password == confirms:
-        # Logica
-    else:
-        return JsonResponse({'detail': 'Contraseñas Diferentes'})
+    if username == '' or first_name == '' or last_name == '' or rol == '':
+        return JsonResponse({'detail': 'Verificar Campos Vacíos'})
+
+    if password != confirms or password == '':
+        return JsonResponse({'detail': 'Contraseñas Inválidas'})
 
 @csrf_protect
 @require_POST
