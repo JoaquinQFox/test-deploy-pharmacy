e51e53b8f0b383fccba5d396eabe8ebbcd30e2b9
JoaquinQFox <jquispebed@unsa.edu.pe>
2025-07-04 21:11:23 -0500

    refactor: se cambia la ubicación de las rutas de app productos

--- pharmacy_sales_system/backend/pharmacy_sales_system/urls.py
+++ pharmacy_sales_system/backend/pharmacy_sales_system/urls.py
@@ -17,13 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from productos import views
-
-router = routers.DefaultRouter()
-router.register(r'producto', views.ProductoViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/', include('productos.urls')),
 ]
