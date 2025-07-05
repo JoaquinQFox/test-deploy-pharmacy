e0434dcb28bb2a0803b3cff78ebfc3a35c712406
leonhatches <jhatches@unsa.edu.pe>
2025-07-04 20:06:01 -0500

    chore: se agrega rest_framework y corsheaders

--- pharmacy_sales_system/backend/pharmacy_sales_system/settings.py
+++ pharmacy_sales_system/backend/pharmacy_sales_system/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'productos',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +50,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200"
 ]
 
 ROOT_URLCONF = 'pharmacy_sales_system.urls'
