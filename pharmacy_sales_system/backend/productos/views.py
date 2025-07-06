df7bb83a0ea86b73927f5ba70b551703a2c4f482
JoaquinQFox <jquispebed@unsa.edu.pe>
2025-07-06 17:34:42 -0500

    refactor: se cambia los permisos para ejecutar acciones crud de productos

--- pharmacy_sales_system/backend/productos/views.py
+++ pharmacy_sales_system/backend/productos/views.py
@@ -4,13 +4,8 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from .serializer import ProductoSerializer
 from .models import Producto
-from .permissions import esRolAdmin
 
 class ProductoViewSet(viewsets.ModelViewSet):
     queryset = Producto.objects.all()
     serializer_class = ProductoSerializer
-
-    def get_permissions(self):
-        if self.request.method in ['POST', 'PUT', 'DELETE']:
-            return [esRolAdmin()]
-        return [IsAuthenticated()]
+    permission_classes = [IsAuthenticated]
