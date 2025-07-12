6e05553040c67fabd3d112c5254d0472f5bc6ca1
leonhatches <jhatches@unsa.edu.pe>
2025-07-12 15:30:03 -0500

    feat(accounts): se agrega el rol de propietario en models

--- pharmacy_sales_system/backend/accounts/models.py
+++ pharmacy_sales_system/backend/accounts/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     ROLES = [
+        ('propietario', 'Propietario'),
         ('admin', 'Administrador'),
         ('vendedor', 'Vendedor'),
     ]
