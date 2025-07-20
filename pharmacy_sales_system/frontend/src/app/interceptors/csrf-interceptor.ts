76d6439292615cfd48b59a143e117014f8555f2a
MauroSullcaMamani <msullcam@unsa.edu.pe>
2025-07-20 14:00:24 -0500

    feat: Agregando encabezado X-CSRFToken para métodos seguros

--- pharmacy_sales_system/frontend/src/app/interceptors/csrf-interceptor.ts
+++ pharmacy_sales_system/frontend/src/app/interceptors/csrf-interceptor.ts
@@ -5,5 +5,17 @@ export const CsrfInterceptor: HttpInterceptorFn = (
   req: HttpRequest<any>,
   next: HttpHandlerFn
 ): Observable<HttpEvent<any>> => {
+  const token = getCookie('csrftoken');
+  const methodsToInclude = ['POST', 'PUT', 'PATCH', 'DELETE'];
+
+  if (token && methodsToInclude.includes(req.method) && !req.headers.has('X-CSRFToken')) {
+    req = req.clone({
+      setHeaders: {
+        'X-CSRFToken': token
+      },
+      withCredentials: true
+    });
+  }
+
   return next(req);
 };
