37cda622ff7500e7add36206c6a79856eb9e8c2d
Calihn1 <ehilacondob@unsa.edu.pe>
2025-07-20 13:29:46 -0500

    feat: se creo un servicio para manejar el inicio de sesion de un usuario

---
+++ pharmacy_sales_system/frontend/src/app/services/auth.ts
@@ -0,0 +1,18 @@
+import { Injectable, signal } from '@angular/core';
+import { Api } from './api';
+import { tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Auth {
+  readonly isAuthenticated = signal(false);
+
+  constructor(private api: Api) {}
+
+  login(credentials: { username: string; password: string }) {
+    return this.api.post<any>('/login/', credentials).pipe(
+      tap(() => this.isAuthenticated.set(true))
+    );
+  }
+}
