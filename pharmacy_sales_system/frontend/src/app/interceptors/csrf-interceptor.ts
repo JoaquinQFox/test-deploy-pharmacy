8121961d23ad589b3f348b65c0a128227a0d4b51
MauroSullcaMamani <msullcam@unsa.edu.pe>
2025-07-20 14:01:47 -0500

    feat: Implementando la función getCookie para obtener el token

--- pharmacy_sales_system/frontend/src/app/interceptors/csrf-interceptor.ts
+++ pharmacy_sales_system/frontend/src/app/interceptors/csrf-interceptor.ts
@@ -19,3 +19,14 @@ export const CsrfInterceptor: HttpInterceptorFn = (
 
   return next(req);
 };
+
+function getCookie(name: string): string | null {
+  const cookies = document.cookie.split(';');
+  for (const cookie of cookies) {
+    const [key, value] = cookie.trim().split('=');
+    if (key === name) {
+      return decodeURIComponent(value);
+    }
+  }
+  return null;
+}
