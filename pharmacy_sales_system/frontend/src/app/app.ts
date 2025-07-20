18655a56ff56b1b1586b7d22bab6030ffb171c97
Calihn1 <ehilacondob@unsa.edu.pe>
2025-07-20 13:34:08 -0500

    feat: cada vez que se ingresa a la pagina el servidor enviara el token para hacer posible el login

--- pharmacy_sales_system/frontend/src/app/app.ts
+++ pharmacy_sales_system/frontend/src/app/app.ts
@@ -1,17 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule, CurrencyPipe } from '@angular/common';
 import { Home } from './componentes/home/home';
 import { Producto } from './services/producto';
+import { Csrf } from './services/csrf';  // o Auth si ahí lo tienes
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, CommonModule, Home, CurrencyPipe],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
-export class App {
+export class App implements OnInit {
   title = 'frontend';
-  constructor(private Producto: Producto) {
+
+  constructor(
+    private producto: Producto,
+    private csrf: Csrf 
+  ) {}
+
+  ngOnInit(): void {
+    this.csrf.getToken().subscribe({
+      next: (res : any) => {
+        console.log('Token CSRF recibido:', res.csrfToken);
+        console.log('Cookies actuales:', document.cookie);
+      },
+      error: (err : any) => {
+        console.error('Error al obtener token CSRF', err);
+      }
+    });
   }
 }
