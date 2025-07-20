bc617452f7b50ee8b119e520eab2930ee8522a60
Calihn1 <ehilacondob@unsa.edu.pe>
2025-07-20 13:36:25 -0500

    feat: se implemento la futura ruta hacia login que sera la principal

--- pharmacy_sales_system/frontend/src/app/app.routes.ts
+++ pharmacy_sales_system/frontend/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { ProductoCreate } from './componentes/producto-create/producto-create';
 import { ProductoEdit } from './componentes/producto-edit/producto-edit';
 import { ProductoList } from './componentes/producto-list/producto-list';
 import { Home } from './componentes/home/home';
+import { Login } from './componentes/login/login';
 
 export const routes: Routes = [
-    { path: '', component: Home },
+    { path: 'home', component: Home },
     { path: 'productos', component: ProductoList },
     { path: 'productos/nuevo', component: ProductoCreate },
     { path: 'productos/editar/:id', component: ProductoEdit },
-];
+    { path: '', component: Login },
+]
