9a8e61ae950d41833aff9c30f7be4fe476c37521
Calihn1 <ehilacondob@unsa.edu.pe>
2025-07-20 13:36:00 -0500

    feat: se implemento que el interceptor se realizara de manera global

--- pharmacy_sales_system/frontend/src/app/app.config.ts
+++ pharmacy_sales_system/frontend/src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { CsrfInterceptor } from './interceptors/csrf-interceptor';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
@@ -8,6 +9,8 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(
+      withInterceptors([CsrfInterceptor])
+    )
   ]
 };
