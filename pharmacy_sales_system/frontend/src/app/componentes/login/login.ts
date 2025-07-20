fdfe0ce78a9d5d962ef3516bf6256fa7b46a7785
MauroSullcaMamani <msullcam@unsa.edu.pe>
2025-07-20 14:05:54 -0500

    feat: Inyectando FormBuilder, Router y servicio Auth

--- pharmacy_sales_system/frontend/src/app/componentes/login/login.ts
+++ pharmacy_sales_system/frontend/src/app/componentes/login/login.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Auth } from '../../services/auth';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
+  private fb = inject(FormBuilder);
+  private router = inject(Router);
+  private auth = inject(Auth);
 
+  form!: FormGroup;
+  errorMessage = '';
+  loading = false;
 }
