7c1bc9790530f22aec2bf1bdd4024c4a2f984725
Calihn1 <ehilacondob@unsa.edu.pe>
2025-07-22 13:55:09 -0500

    feat: se actualizaron y ahora las rutas estan protegidas

--- pharmacy_sales_system/frontend/src/app/app.routes.ts
+++ pharmacy_sales_system/frontend/src/app/app.routes.ts
@@ -1,14 +1,17 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth-guard';
 import { ProductoCreate } from './componentes/producto-create/producto-create';
 import { ProductoEdit } from './componentes/producto-edit/producto-edit';
 import { ProductoList } from './componentes/producto-list/producto-list';
 import { Home } from './componentes/home/home';
 import { Login } from './componentes/login/login';
 
+
 export const routes: Routes = [
-    { path: 'home', component: Home },
-    { path: 'productos', component: ProductoList },
-    { path: 'productos/nuevo', component: ProductoCreate },
-    { path: 'productos/editar/:id', component: ProductoEdit },
-    { path: '', component: Login },
-]
+  { path: 'home', component: Home, canActivate: [AuthGuard] },
+  { path: 'productos', component: ProductoList, canActivate: [AuthGuard] },
+  { path: 'productos/nuevo', component: ProductoCreate, canActivate: [AuthGuard] },
+  { path: 'productos/editar/:id', component: ProductoEdit, canActivate: [AuthGuard] },
+  { path: '', component: Login }, 
+  { path: '**', redirectTo: '' },
+];
