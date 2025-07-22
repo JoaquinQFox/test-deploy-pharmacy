ea60d7e831eba19c9e641fcd45875d8004df9411
MauroSullcaMamani <msullcam@unsa.edu.pe>
2025-07-22 15:17:37 -0500

    feat: Implementando la carga y visualización de datos para edición de usuario

--- pharmacy_sales_system/frontend/src/app/componentes/user-edit/user-edit.ts
+++ pharmacy_sales_system/frontend/src/app/componentes/user-edit/user-edit.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { User } from '../../services/user';
 
 @Component({
   selector: 'app-user-edit',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './user-edit.html',
-  styleUrl: './user-edit.css'
+  styleUrls: ['./user-edit.css']
 })
-export class UserEdit {
+export class UserEdit implements OnInit {
+  userForm!: FormGroup;
+  userId!: number;
+  errorMessage: string | null = null;
+  successMessage: string | null = null;
+
+  constructor(
+    private route: ActivatedRoute,
+    private fb: FormBuilder,
+    private user: User,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.userId = Number(this.route.snapshot.paramMap.get('id'));
+
+    this.userForm = this.fb.group({
+      username: ['', Validators.required],
+      first_name: ['', Validators.required],
+      last_name: ['', Validators.required],
+      rol: ['', Validators.required]
+    });
+
+    this.user.getUser(this.userId).subscribe({
+      next: user => this.userForm.patchValue(user),
+      error: err => this.errorMessage = err.error.detail || 'Error al cargar usuario'
+    });
+  }
 
 }
