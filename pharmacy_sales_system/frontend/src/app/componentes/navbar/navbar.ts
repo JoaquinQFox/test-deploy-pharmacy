3581a005e084d8c4e191d180ab9cec6e9c64b5f2
Calihn1 <ehilacondob@unsa.edu.pe>
2025-07-22 13:29:08 -0500

    feat: se implemento un componente navbar donde se podra cerrar sesion

---
+++ pharmacy_sales_system/frontend/src/app/componentes/navbar/navbar.ts
@@ -0,0 +1,21 @@
+import { Component, computed, signal } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Auth } from '../../services/auth'
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './navbar.html',
+  styleUrls: ['./navbar.css']
+})
+export class Navbar {
+  showMenu = signal(false);
+
+  constructor(
+    private auth: Auth,
+    private router: Router
+  ) {}
+
+  
