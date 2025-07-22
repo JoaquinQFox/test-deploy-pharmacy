b026c40c8573c079450f9988f41782457d1f43c7
MauroSullcaMamani <msullcam@unsa.edu.pe>
2025-07-22 15:29:53 -0500

    feat: Creando las interfaces ventaCreada y ventaD

--- pharmacy_sales_system/frontend/src/app/services/venta.ts
+++ pharmacy_sales_system/frontend/src/app/services/venta.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface VentaCreada {
+  id: number;
+  usuario: {
+    first_name: string;
+    last_name: string;
+  };
+  fecha: string;
+  descuento: string;
+  total_bruto: string;
+  total_neto: string;
+  detalles: any[];
+}
+
+export interface VentaD {
+  descuento: number;
+  detalles: {
+    producto: number;
+    cantidad: number;
+  }[];
+}
 
 @Injectable({
   providedIn: 'root'
